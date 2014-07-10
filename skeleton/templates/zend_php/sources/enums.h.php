b09f9bda737988c07475f156932f1de9b8bdbff9
jgmdev <jgmdev@gmail.com>
2014-07-10 14:46:52 -0400

    Some more work on enums integration.

--- skeleton/templates/zend_php/sources/enums.h.php
+++ skeleton/templates/zend_php/sources/enums.h.php
@@ -30,20 +30,35 @@ static int peg_enum_apply_set(long *option TSRMLS_DC, int num_args, va_list args
 
 /* enum object */
 struct _peg_enum_object {
-	zend_object std;
-	zend_bool   is_constructed;
-	long        value;
-	HashTable  *elements;
+    zend_object std;
+    zend_bool   is_constructed;
+    long        value;
+    HashTable  *elements;
 };
 
 /* enum struct object */
 typedef struct _peg_enum_object peg_enum_object;
 
 #define PHP_PEG_EXCEPTIONS \
-	zend_error_handling error_handling; \
-	zend_replace_error_handling(EH_THROW, spl_ce_InvalidArgumentException, &error_handling TSRMLS_CC);
+    zend_error_handling error_handling; \
+    zend_replace_error_handling(EH_THROW, spl_ce_InvalidArgumentException, &error_handling TSRMLS_CC);
 
 #define PHP_PEG_RESTORE_ERRORS \
-	zend_restore_error_handling(&error_handling TSRMLS_CC);
+    zend_restore_error_handling(&error_handling TSRMLS_CC);
+    
+/* {{{ methods prototype */
+PHP_METHOD(Peg_Enum, __construct);
+PHP_METHOD(Peg_Enum, getName);
+PHP_METHOD(Peg_Enum, getElements);
+/* }}} */
+    
+/* {{{ class methods */
+static const zend_function_entry peg_enum_methods[] = {
+    PHP_ME(Peg_Enum, __construct, Enum___construct_args, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
+    PHP_ME(Peg_Enum, getName, NULL, ZEND_ACC_PUBLIC)
+    PHP_ME(Peg_Enum, getElements, NULL, ZEND_ACC_PUBLIC)
+    ZEND_FE_END
+};
+/* }}} */
     
 #endif //PEG_ENUMS_H

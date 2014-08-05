debcbc603a9fb65cb8695f3a78bed51bb543f65c
Patrick D. Andersen <pdandersen@makoto-ub.scooby.doobies>
2014-08-05 15:07:16 -0400

    Removed all references to the CLI from Application.php

--- src/Application.php
+++ src/Application.php
@@ -12,43 +12,7 @@ namespace Peg\Lib;
  */
 class Application
 {
-
-    /**
-     * Reference to the global parser.
-     * @var \Peg\Lib\CommandLine\Parser
-     */
-    private static $cli_parser;
-
-    /**
-     * Reference to the plugins loader.
-     * @var \Peg\Lib\Plugins\Loader;
-     */
-    private static $plugin_loader;
-
-    /**
-     * Reference to the help command.
-     * @var \Peg\Lib\Command\Help
-     */
-    private static $help_command;
-
-    /**
-     * Reference to the init command.
-     * @var \Peg\Lib\Command\Init
-     */
-    private static $init_command;
-
-    /**
-     * Reference to the parse command.
-     * @var \Peg\Lib\Command\Parse
-     */
-    private static $parse_command;
-
-    /**
-     * Reference to the generate command.
-     * @var \Peg\Lib\Command\Generate
-     */
-    private static $generate_command;
-
+    
     /**
      * Reference to the definition's symbols object.
      * @var \Peg\Lib\Definitions\Symbols
@@ -64,25 +28,6 @@ class Application
      */
     public static function Initialize()
     {
-        self::$cli_parser = new CommandLine\Parser;
-
-        // Set Application details
-        self::$cli_parser->application_name = "peg";
-        self::$cli_parser->application_version = "1.0";
-        self::$cli_parser->application_description = t("PHP Extension Generator (http://github.com/peg-org/peg-src)");
-
-        // Create commands
-        self::$help_command = new Command\Help;
-        self::$init_command = new Command\Init;
-        self::$parse_command = new Command\Parse;
-        self::$generate_command = new Command\Generate;
-
-        // Register command operations
-        self::$cli_parser->RegisterCommand(self::$help_command);
-        self::$cli_parser->RegisterCommand(self::$init_command);
-        self::$cli_parser->RegisterCommand(self::$parse_command);
-        self::$cli_parser->RegisterCommand(self::$generate_command);
-
         // Initialize the plugin loader and try to load any plugins.
         self::$plugin_loader = new Plugins\Loader();
 
@@ -147,52 +92,7 @@ class Application
 
         throw new Exception("Skeleton path not found.");
     }
-
-    /**
-     * Gets the global command line parser.
-     * @return \Peg\Lib\CommandLine\Parser
-     */
-    public static function &GetCLIParser()
-    {
-        return self::$cli_parser;
-    }
-
-    /**
-     * Gets a reference to init command currently used by peg.
-     * @return \Peg\Lib\Command\Init
-     */
-    public static function &GetInitCommand()
-    {
-        return self::$init_command;
-    }
-
-    /**
-     * Gets a reference to help command currently used by peg.
-     * @return \Peg\Lib\Command\Help
-     */
-    public static function &GetHelpCommand()
-    {
-        return self::$help_command;
-    }
-
-    /**
-     * Gets a reference to parse command currently used by peg.
-     * @return \Peg\Lib\Command\Parse
-     */
-    public static function &GetParseCommand()
-    {
-        return self::$parse_command;
-    }
-
-    /**
-     * Gets a reference to generate command currently used by peg.
-     * @return \Peg\Lib\Command\Parse
-     */
-    public static function &GetGenerateCommand()
-    {
-        return self::$generate_command;
-    }
-
+    
     /**
      * Loads definitions files if not yet loaded and returns a reference to a
      * symbols object that can be used throught the application.
